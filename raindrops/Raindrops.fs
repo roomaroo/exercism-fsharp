9cba22ba230ae2722dfdc44257f108817fcff14b
Oliver Maunder <olly@roomaroo.co.uk>
2018-10-04 10:27:28 +0200

    Reworked raindrops

--- raindrops/Raindrops.fs
+++ raindrops/Raindrops.fs
@@ -1,12 +1,8 @@
 module Raindrops
 
-let raindropSounds = [(3, "Pling"); (5, "Plang"); (7, "Plong")]  
-
 let convert number = 
-    raindropSounds
-    |> List.filter (fun (factor, _) -> number % factor = 0)
-    |> List.map snd
-    |> List.fold (+) ""
+    [(3, "Pling"); (5, "Plang"); (7, "Plong")]
+    |> List.choose (fun (factor, sound) -> if number % factor = 0 then Some sound else None)
     |> function
-       | "" -> string number
-       | sounds -> sounds
+       | [] -> string number
+       | sounds -> List.reduce (+) sounds
