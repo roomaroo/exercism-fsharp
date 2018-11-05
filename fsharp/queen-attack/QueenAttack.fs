9f01dfb627c8630170994239d6bf07202394bc45
omaunder <oliver.maunder@greenpowermonitor.com>
2018-11-05 12:23:20 +0100

    Added Queen Attack

---
+++ fsharp/queen-attack/QueenAttack.fs
@@ -0,0 +1,14 @@
+﻿module QueenAttack
+
+let create (row, col) = 
+    let boardSize = 8
+    let validCoord x = 
+        x >=0 && x < boardSize
+        
+    validCoord row && validCoord col
+
+let canAttack (row1, col1) (row2, col2) = 
+    row1 = row2
+    || col1 = col2
+    || abs(row1 - row2) = abs(col1 - col2)
+
