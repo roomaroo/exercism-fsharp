6d0d91a50e11725e641f33104fd42125d6a2a211
Olly Maunder <oliver.maunder@greenpowermonitor.com>
2019-01-28 10:26:32 +0100

    Solution for Rotational Cipher

---
+++ fsharp/rotational-cipher/RotationalCipher.fs
@@ -0,0 +1,24 @@
+﻿module RotationalCipher
+
+open System
+
+let letters = List.ofSeq "abcdefghijklmnopqrstuvwxyz"
+let isLetter c = Char.IsLetter c
+let isUpper c = Char.ToUpper(c) = c
+
+let encode shiftKey c = 
+    if isLetter c then
+        letters 
+        |> List.findIndex ((=) (Char.ToLower(c)))
+        |> (+) shiftKey 
+        |> fun n -> letters.[n % 26]
+        |> fun x -> if isUpper c then Char.ToUpper(x) else x
+    else
+        c    
+
+let rotate shiftKey text = 
+    text
+    |> Seq.map (encode shiftKey)
+    |> String.Concat
+
+    
