44558356fe0aa601084173e17aa8be5baad28390
Olly Maunder <oliver.maunder@greenpowermonitor.com>
2019-01-28 11:02:24 +0100

    Minesweeper solution

---
+++ fsharp/minesweeper/Minesweeper.fs
@@ -0,0 +1,48 @@
+﻿module Minesweeper
+
+let findMines input = 
+    input 
+    |> Seq.mapi (fun row line -> 
+                    line
+                    |> Seq.mapi (fun col c ->
+                                    if c = '*' then Some (row, col) else None))                      
+    |> Seq.collect (Seq.choose id)
+    |> List.ofSeq
+
+
+let annotate input = 
+    let mines = findMines input
+    let isMine coord = List.exists ((=) coord) mines
+
+    let surroundingCells (row, col) =
+        [
+            (row - 1, col - 1);
+            (row - 1, col);
+            (row - 1, col + 1);
+            (row, col - 1);
+            (row, col + 1);
+            (row + 1, col - 1);
+            (row + 1, col);
+            (row + 1, col + 1);
+        ]
+
+    let countMines coord = 
+        surroundingCells coord
+        |> List.filter isMine
+        |> List.length
+
+    let annotateCell coord = 
+        if isMine coord then
+            "*"
+        else 
+            let count = countMines coord 
+            if count > 0 then string count else " "
+
+    input 
+    |> List.mapi (fun i row -> 
+                        row
+                        |> Seq.mapi (fun j _ -> annotateCell (i, j))  
+                        |> String.concat "")
+
+
+
