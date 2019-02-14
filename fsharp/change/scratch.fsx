488863975a758d1804ea889055c8477b4a96dba4
Olly Maunder <oliver.maunder@greenpowermonitor.com>
2019-02-14 19:06:50 +0100

    Started work on change

---
+++ fsharp/change/scratch.fsx
@@ -0,0 +1,22 @@
+let largestCoin coins target = 
+    coins |> List.tryFind (fun c -> c <= target)
+
+let cons a b = 
+    match (a, b) with
+    | _, None -> None
+    | a, Some b -> a :: b |> Some
+
+let findFewestCoins coins target =
+    let coins = List.rev coins
+    let rec findFewestCoinsImpl target =
+        if target = 0 then
+            Some []
+        else           
+            match largestCoin coins target with
+            | None -> None
+            | Some c -> cons c (findFewestCoinsImpl (target - c))
+
+    findFewestCoinsImpl target        
+
+
+findFewestCoins (List.rev [1;2;5;10;25;100]) 25
