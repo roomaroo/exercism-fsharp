e47c37defcc44f1622f32614cf8f148b04978a12
Olly Maunder <oliver.maunder@greenpowermonitor.com>
2019-03-06 10:56:42 +0100

    Making account balance mutable (ugh) with locking

--- fsharp/bank-account/BankAccount.fs
+++ fsharp/bank-account/BankAccount.fs
@@ -1,22 +1,32 @@
 ﻿module BankAccount
+open System.Threading
+open System
 
-open System.Collections.Concurrent
+type Account = {mutable Balance: decimal option; Mutex: obj}
 
-type AccountState = Open | Closed
-type Transaction = { Amount: decimal }
-type Account = {State: AccountState; Transactions: ConcurrentBag<Transaction>}
+let lock (f: Account -> unit) account = 
+    if Monitor.TryEnter(account.Mutex, TimeSpan.FromMilliseconds(100.0)) then
+        f account
+        Monitor.Exit account.Mutex
+        account
+    else
+        failwith "Failed to lock account"
 
-let mkBankAccount() = {State = Closed; Transactions = ConcurrentBag<_>()}
+let mkBankAccount() = {Balance = None; Mutex = obj()}
 
-let openAccount account = {account with State = Open}
-
-let closeAccount account = {account with State = Closed}
+let openAccount account =
+    lock (fun a -> a.Balance <- defaultArg account.Balance 0.0m |> Some) account
+    
+let closeAccount account = 
+    lock (fun a -> a.Balance <- None) account
 
 let getBalance account = 
-    match account.State with
-    | Open -> account.Transactions |> Seq.sumBy (fun t -> t.Amount) |> Some
-    | Closed -> None
+    account.Balance
 
 let updateBalance change account = 
-    account.Transactions.Add({Amount = change})
-    account
+    let update account = 
+        match account.Balance with
+        | Some x -> account.Balance <- Some(x + change)
+        | None -> ()
+
+    lock update account
