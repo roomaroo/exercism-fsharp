69daf3e3b5eb3bc0a1280b1201b73acfb3be07c4
Olly Maunder <oliver.maunder@greenpowermonitor.com>
2019-03-18 11:47:39 +0100

    Grains solution

--- fsharp/grains/Grains.fs
+++ fsharp/grains/Grains.fs
@@ -1,4 +1,16 @@
 ﻿module Grains
+open System.Numerics
 
-let square (n: int): Result<uint64,string> = failwith "You need to implement this function."
-let total: Result<uint64,string> = failwith "You need to implement this function."
+let squareImpl n = BigInteger.Pow(bigint 2, n - 1)
+
+let square (n: int): Result<uint64,string> = 
+    match n with
+    | x when x < 1 || x > 64 -> Error "square must be between 1 and 64"
+    | n -> n |> squareImpl |> uint64 |> Ok
+
+let total: Result<uint64,string> = 
+    [1..64]
+    |> List.map squareImpl
+    |> List.fold (fun a b -> BigInteger.Add(a, b)) (BigInteger.Zero)
+    |> uint64
+    |> Ok
