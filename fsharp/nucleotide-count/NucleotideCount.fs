6c8a5edc3b37785389275e48889d5add75ae0eb7
Olly Maunder <oliver.maunder@greenpowermonitor.com>
2019-04-10 18:22:49 +0200

    Solution for nucleotide count

---
+++ fsharp/nucleotide-count/NucleotideCount.fs
@@ -0,0 +1,24 @@
+﻿module NucleotideCount
+
+let nucleotideCounts (strand: string): Option<Map<char, int>> = 
+
+    let types = ['A';'C';'G';'T']
+
+    let count c = 
+        strand |> Seq.filter ((=) c) |> Seq.length
+
+    let isValid c = List.contains c types
+
+    if String.forall isValid strand then 
+        types
+        |> List.map (fun t -> (t, count t))
+        |> Map.ofList
+        |> Some  
+
+    else None    
+
+    
+
+    
+
+
