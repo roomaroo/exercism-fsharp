639d2eeb3223ad3388fa8f2b6a11011c95853e74
Maunder <oliver.maunder@dnvgl.com>
2019-06-19 16:53:54 +0100

    Solution for Diffie-Hellman

---
+++ fsharp/diffie-hellman/DiffieHellman.fs
@@ -0,0 +1,22 @@
+﻿module DiffieHellman
+
+open System.Security.Cryptography
+open System.Numerics
+
+let generator = new RNGCryptoServiceProvider()
+
+let rec createKey (max: BigInteger) = 
+    let bytes = Array.create 8 0uy
+    generator.GetBytes(bytes)
+    let key = BigInteger(bytes) % max
+
+    if (key > 1I) then key else (createKey max)
+
+let privateKey (primeP : BigInteger) = 
+    createKey primeP
+
+let publicKey primeP primeG privateKey = 
+    BigInteger.ModPow(primeG, privateKey, primeP)
+
+let secret primeP publicKey privateKey = 
+    BigInteger.ModPow(publicKey, privateKey, primeP)
