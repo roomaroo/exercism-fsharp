e7ccbb1eadf7693a2e524b687b7d1a1abe1e1669
Maunder <oliver.maunder@dnvgl.com>
2019-07-12 11:31:46 +0100

    Dominoes solution

--- fsharp/dominoes/Dominoes.fs
+++ fsharp/dominoes/Dominoes.fs
@@ -1,3 +1,28 @@
 ﻿module Dominoes
-    
-let rec canChain input = failwith "You need to implement this function."
+
+let selectNthDomino dominoes n =
+    let nth = List.item n dominoes
+    let others =
+        dominoes
+        |> List.mapi (fun i d -> if i = n then None else Some d)
+        |> List.choose id
+
+    (nth, others)
+
+let makeChain (x1, y1) others =
+    match others with
+    | (x2, y2)::xs when y1 = x2 -> (x1, y2)::xs |> Some
+    | (x2, y2)::xs when x1 = x2 -> (y1, y2)::xs |> Some
+    | _ -> None
+
+
+let rec canChain input = 
+    match input with
+    | [] -> true
+    | [(x, y)] when x = y -> true
+    | _ -> 
+        [0..(List.length input) - 1]
+        |> List.map (selectNthDomino input)
+        |> List.choose (fun (x, xs) -> makeChain x xs) 
+        |> List.map canChain
+        |> List.exists id
