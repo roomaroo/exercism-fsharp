b814e20401909bb2ec226ec24eb3e3a06b3c5782
Olly Maunder <olly@roomaroo.co.uk>
2019-07-25 14:35:03 +0100

    Doubly-linked list implementation

--- fsharp/linked-list/LinkedList.fs
+++ fsharp/linked-list/LinkedList.fs
@@ -1,13 +1,52 @@
 module LinkedList
 
-let mkLinkedList () = failwith "You need to implement this function."
+type Node(value) =
+    member val Value = value
+    member val Prev: Node option = None with get, set
+    member val Next: Node option = None with get, set
 
-let addToEmpty newValue linkedList = failwith "You need to implement this function."
+type LinkedList() = 
+    member val Head: Node option = None with get, set
 
-let pop linkedList = failwith "You need to implement this function."
+    member this.Last() =
+        let rec lastImpl (node: Node) =
+            match node.Next with
+            | Some n -> lastImpl n
+            | None -> node
 
-let shift linkedList = failwith "You need to implement this function."
+        Option.map lastImpl this.Head        
 
-let push newValue linkedList = failwith "You need to implement this function."
+let mkLinkedList () = LinkedList()
 
-let unshift newValue linkedList = failwith "You need to implement this function."
+let addToEmpty newValue (linkedList: LinkedList) = 
+    linkedList.Head <- Node(newValue) |> Some
+
+let pop (linkedList: LinkedList) = 
+    match linkedList.Last() with
+    | Some node -> 
+        Option.map (fun (p: Node) -> p.Next <- None) node.Prev |> ignore
+        node.Value
+    | None -> failwith "Can't pop an empty list"
+
+let shift (linkedList: LinkedList) = 
+    match linkedList.Head with
+    | Some node -> 
+        Option.map (fun (n: Node) -> n.Prev <- None) node.Next |> ignore
+        linkedList.Head <- node.Next
+        node.Value
+    | None -> failwith "Can't shift an empty list"
+
+let push newValue (linkedList: LinkedList) = 
+    match linkedList.Last() with
+    | Some n ->
+        let newNode = Node(newValue, Prev = Some n) |> Some
+        n.Next <- newNode
+    | None -> addToEmpty newValue linkedList    
+
+let unshift newValue (linkedList: LinkedList) =
+    match linkedList.Head with
+    | Some n ->
+        let newNode = Node(newValue, Next = Some n) |> Some
+        n.Prev <- newNode
+        linkedList.Head <- newNode
+    | None -> addToEmpty newValue linkedList   
